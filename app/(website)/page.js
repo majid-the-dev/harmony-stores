236687b46ba504920838b8f8725520cc775fd0c1
majid-the-dev <contactmajidoladepo@gmail.com>
2024-06-17 11:43:57 -0700

    commit message

--- app/(website)/page.js
+++ app/(website)/page.js
@@ -29,7 +29,7 @@ export default function Home() {
       {/* <NewArrivals /> */}
 
       {/* Trending Products */}
-      <TrendingProducts />
+      {/* <TrendingProducts /> */}
 
       <BrandMarquee />
 
