b0a16e8b7a9fb4825eb4e4b2af3a5126e8c85c86
majid-the-dev <contactmajidoladepo@gmail.com>
2024-06-17 08:37:15 -0700

    commit message

--- app/api/upload/route.js
+++ app/api/upload/route.js
@@ -9,8 +9,8 @@ export const POST = async (req) => {
     const s3Client = new S3Client({
         region: "eu-north-1",
         credentials: {
-            accessKeyId: process.env.AWS_ACCESS_KEY,
-            secretAccessKey: process.env.AWS_SECRET_KEY
+            accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+            secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY
         },
     });
 
